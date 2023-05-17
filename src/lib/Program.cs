891942434d242e3d4cb34cde68e9991be8a6aba0
Alecio Furanze <alecio@kezero.com>
2023-05-18 00:53:37 +0200

    ``update`` create startup functionality

--- src/lib/Program.cs
+++ src/lib/Program.cs
@@ -2,8 +2,35 @@
 
 public class Program
 {
+    private const string initError = "[ERROR]\n\nExample 1:\n\tdotnet net run <ip> <port>\n\nExample 2:\n\tdotnet net run dev\n";
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Console application");
+        string address;
+        int port;
+
+        try
+        {
+            if (args[0].ToLower() == "dev")
+            {
+                address = "0.0.0.0";
+                port = 10101;
+            }
+            else
+            {
+                address = args[0];
+                port = int.Parse(args[1]);
+            }
+        }
+        catch
+        {
+            Console.WriteLine(initError);
+            return;
+        }
+
+        _ = new MasterServer(address, port);
+
+        Console.WriteLine("[STARTED] KEY {ENTER} TO STOP");
+        Console.ReadLine();
     }
 }
