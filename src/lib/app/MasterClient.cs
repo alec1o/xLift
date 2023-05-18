efbe18bb6823bbd2a087859e8f7db233bbf23268
Alecio Furanze <alecio@kezero.com>
2023-05-18 03:34:25 +0200

    ``update`` sample jwt verifying

--- src/lib/app/MasterClient.cs
+++ src/lib/app/MasterClient.cs
@@ -1,13 +1,22 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
 namespace Sisma;
 
 public class MasterClient
 {
     public readonly string rawJwt;
+    private readonly JwtData jwtData;
+
     public bool isConnected { get; private set; }
 
-    public MasterClient(string jwt)
+    public MasterClient(string rawJwt, JwtData jwtData)
     {
-        this.rawJwt = jwt;
+        this.rawJwt = rawJwt;
+        this.jwtData = jwtData;
     }
 
     public void Close()
@@ -25,9 +34,29 @@ public class MasterClient
 
     }
 
-    public static bool VerifyJwt(string jwt)
+    public static JwtData? VerifyJwt(string rawJwt)
     {
-        // todo: verify jwt token with env key.
-        return jwt == null;
+        if (rawJwt == null) return null;
+
+        var secret = "password-here";
+        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+        var tokenParams = new TokenValidationParameters()
+        {
+            IssuerSigningKey = secretKey
+        };
+
+        try
+        {
+            var handle = new JwtSecurityTokenHandler();
+            _ = handle.ValidateToken(rawJwt, tokenParams, out var validatedToken);
+
+            string json = rawJwt.Split('.')[2];
+
+            return JsonSerializer.Deserialize<JwtData>(json);
+        }
+        catch
+        {
+            return null;
+        }
     }
 }
