a44c53ded55e22fdd0ef431efe576dc7385e8dd3
Alecio Furanze <alecio@kezero.com>
2023-05-20 00:56:56 +0200

    include token input on login page

--- src/web/src/pages/Home/index.tsx
+++ src/web/src/pages/Home/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
             <main className={style.main}>
                 <form className={style.form} action="" method="post">
                     <h1 className={style.title}>Sisma</h1>
-
-                    <input className={style.input} required placeholder='IP Address' type="text" name="ip" min={4} />
+                    <input className={style.input} required placeholder='IP Address' type="text" name="ip" minLength={4} maxLength={128} />
                     <input className={style.input} required placeholder='Port' type="number" name="port" min={1} max={65535} />
+                    <input className={style.input} required placeholder='Token' type="text" name="token" minLength={1} maxLength={512} />
                     <input className={style.input} id={style.button} type="submit" value="ENTER" />
                 </form>
             </main>
