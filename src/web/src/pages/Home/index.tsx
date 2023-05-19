b293dd103d52b051875fe01532f622067ef5ce1e
Alecio Furanze <alecio@kezero.com>
2023-05-20 00:44:04 +0200

    create home page

--- src/web/src/pages/Home/index.tsx
+++ src/web/src/pages/Home/index.tsx
@@ -6,8 +6,14 @@ export default function Home() {
         <div className={style.app}>
             <Header />
 
-            <main>
-                <h1 className={style.title}>Home</h1>
+            <main className={style.main}>
+                <form className={style.form} action="" method="post">
+                    <h1 className={style.title}>Sisma</h1>
+
+                    <input className={style.input} required placeholder='IP Address' type="text" name="ip" min={4} />
+                    <input className={style.input} required placeholder='Port' type="number" name="port" min={1} max={65535} />
+                    <input className={style.input} id={style.button} type="submit" value="ENTER" />
+                </form>
             </main>
         </div>
     )
