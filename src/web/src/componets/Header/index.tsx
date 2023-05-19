f7f94b14c39a26fbec744b35d9b1cc36e8723366
Alecio Furanze <alecio@kezero.com>
2023-05-19 23:33:12 +0200

    create header component

--- src/web/src/componets/Header/index.tsx
+++ src/web/src/componets/Header/index.tsx
@@ -1,9 +1,18 @@
 import style from "./style.module.css"
+import * as bs from "react-icons/bs"
+import * as gi from "react-icons/gi"
 
 export default function Header() {
     return (
         <header className={style.header}>
-            <h1>Header</h1>
+            <nav className={style.nav}>
+                <a id={style.logo} href="/">SISMA</a>
+            </nav>
+
+            <nav className={style.nav}>
+                <a className={style.link} target="_blank" href="https://github.com/alec1o/Sisma"><gi.GiWhiteBook color="white" />Docs</a>
+                <a className={style.link} target="_blank" href="https://github.com/alec1o/Sisma"><bs.BsGithub color="white" />GitHub</a>
+            </nav>
         </header>
     )
 }
