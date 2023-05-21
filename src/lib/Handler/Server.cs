ee7c9bb1a566e37235291b5151cb6fe85d62f13b
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:32:05 +0200

    Create Server.cs

---
+++ src/lib/Handler/Server.cs
@@ -0,0 +1,42 @@
+﻿using Sisma.Models;
+using System.Net;
+using WatsonWebsocket;
+
+namespace Sisma.Handler;
+
+public class Server
+{
+    public readonly (string address, int port) Host;
+    public readonly List<Room> Rooms;
+    public readonly List<Match> Matches;
+    public readonly List<Client> Clients;
+    public readonly WatsonWsServer Socket;
+
+    public Server((string address, int port) host)
+    {
+        Host = host;
+        Rooms = new List<Room>();
+        Matches = new List<Match>();
+        Clients = new List<Client>();
+        Socket = new WatsonWsServer(host.address, host.port, false);
+
+        Socket.ClientConnected += (_, connection) =>
+        {
+            Auth(connection);
+        };
+
+        Socket.ServerStopped += (_, o) =>
+        {
+            Clients.Clear();
+            Console.WriteLine("[WEBSOCKET SERVER CLOSED]");
+        };
+
+        Socket.Start();
+        Console.WriteLine($"[WEBSOCKET SERVER STARTED] {host}");
+    }
+
+    private void Auth(ConnectionEventArgs client)
+    {
+        Console.WriteLine($"[CLIENT] Connected ({client.Client.Ip}, {client.Client.Port})");
+    }
+}
