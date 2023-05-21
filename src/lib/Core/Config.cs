66c978acc7f64aad84109a2679c37ae74f28a004
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:32:16 +0200

    Create Config.cs

---
+++ src/lib/Core/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Sisma.Core;
+
+public static class Config
+{
+    public static readonly string SISMA_ROOT_KEY =
+        Environment.GetEnvironmentVariable("SISMA_ROOT_KEY", EnvironmentVariableTarget.User)
+        ?? throw new ArgumentNullException("SISMA_ROOT_KEY Not found");
+}
