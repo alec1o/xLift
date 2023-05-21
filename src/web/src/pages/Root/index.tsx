7715aa703ba1562f35f5cf36c11b58174232ef04
Alecio Furanze <alecio@kezero.com>
2023-05-21 07:01:16 +0200

    update style of root page

--- src/web/src/pages/Root/index.tsx
+++ src/web/src/pages/Root/index.tsx
@@ -31,36 +31,39 @@ export default function Root() {
             <Header />
             <main id={style.app} className={homeStyle.app}>
                 <div className={style.main}>
-                    <ul className={style.mainGroup}>
-                        <li>
-                            <h2 className={style.title}>Modes</h2>
-                        </li>
-                        {modes.map((e) =>
-                            <li>
-                                <article className={style.mode}>
-                                    <span id={style.modeName} className={style.modeElement}>Mode: {e.MODE_NAME}</span>
-                                    <div className={style.modeArea}>
-                                        <span className={style.modeElement}>Image: {e.CONTAINER_IMAGE}</span>
-                                        <span className={style.modeElement}>Params: {e.CONTAINER_PARAM}</span>
-                                    </div>
-                                </article>
-                            </li>
-                        )}
-                    </ul>
-                    <ul className={style.mainGroup}>
-                        <li>
-                            <h2 className={style.title}>Status</h2>
-                        </li>
-                        <li className={style.states}>Rooms: {states.ONLINE_ROOM}</li>
-                        <li className={style.states}>Online user: {states.ONLINE_USER}</li>
-                        <li className={style.states}>User on room: {states.USER_AT_MATCH}</li>
-                        <li className={style.states}>rooms created: {states.ROOMS_CREATED}</li>
-                    </ul>
-                    <ul id={style.inspector} className={style.mainGroup}>
-                        <li>
-                            <h2 className={style.title}>Inspector</h2>
-                        </li>
-                    </ul>
+
+                    <div className={style.area}>
+                        <h2 className={style.title}>Modes</h2>
+                        <ul className={style.mainGroup}>
+                            {modes.map((e) =>
+                                <li>
+                                    <article className={style.mode}>
+                                        <span id={style.modeName} className={style.modeElement}>Mode: {e.MODE_NAME}</span>
+                                        <div className={style.modeArea}>
+                                            <span className={style.modeElement}>Image: {e.CONTAINER_IMAGE}</span>
+                                            <span className={style.modeElement}>Params: {e.CONTAINER_PARAM}</span>
+                                        </div>
+                                    </article>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+
+                    <div className={style.area}>
+                        <h2 className={style.title}>Status</h2>
+                        <ul className={style.mainGroup}>
+                            <li className={style.states}>Rooms: {states.ONLINE_ROOM}</li>
+                            <li className={style.states}>Online user: {states.ONLINE_USER}</li>
+                            <li className={style.states}>User on room: {states.USER_AT_MATCH}</li>
+                            <li className={style.states}>rooms created: {states.ROOMS_CREATED}</li>
+                        </ul>
+                    </div>
+
+                    <div id={style.inspector} className={style.area}>
+                        <h2 className={style.title}>Inspector</h2>
+                        <ul className={style.mainGroup}>
+                        </ul>
+                    </div>
                 </div>
             </main>
         </>
