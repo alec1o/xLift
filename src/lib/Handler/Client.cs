1e9efa880c538cb540f8bd91c9193e560edfb407
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:32:08 +0200

    Create Client.cs

---
+++ src/lib/Handler/Client.cs
@@ -0,0 +1,32 @@
+﻿using Sisma.Models;
+using System.Net.WebSockets;
+using System.Text;
+using WatsonWebsocket;
+
+namespace Sisma.Handler
+{
+    public class Client
+    {
+        public User User { get; set; }
+        public Match? Match { get; set; }
+        public ConnectionEventArgs Connection { get; set; }
+        public Server Server { get; set; }
+
+        public Client(User user, ConnectionEventArgs connection, Server server)
+        {
+            User = user;
+            Server = server;
+            Connection = connection;
+        }
+
+        public void Send(byte[] buffer)
+        {
+            Server.Socket.SendAsync(Connection.Client.Guid, buffer, WebSocketMessageType.Text);
+        }
+
+        public void Send(string buffer)
+        {
+            Send(Encoding.UTF8.GetBytes(buffer));
+        }
+    }
+}
