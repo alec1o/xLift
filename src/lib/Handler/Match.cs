7d4beeadddb167a36edeeb6113a70cdd0f787cd9
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:31:57 +0200

    Create Match.cs

---
+++ src/lib/Handler/Match.cs
@@ -0,0 +1,16 @@
+﻿using Sisma.Models;
+
+namespace Sisma.Handler;
+
+public class Match
+{
+    public bool Opened { get; set; }
+    public Room Room { get; set; }
+    public List<User> Users { get; set; }
+
+    public Match(Room room)
+    {
+        Room = room;
+        Users = new List<User>();
+    }
+}
