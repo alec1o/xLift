2da5dac8b303a0c81415f4535729c3bf10421c3e
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:31:30 +0200

    Create Room.cs

---
+++ src/lib/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿namespace Sisma.Models;
+
+public class Room
+{
+    public string UID { get; set; }
+    public string Mode { get; set; }
+    public string ContainerImage { get; set; }
+    public string ContainerParam { get; set; }
+    public Port[] ContainerPorts { get; set; }
+    public int MatchTimeout { get; set; }
+    public int MinUser { get; set; }
+    public int MaxUser { get; set; }
+
+    public Room(string uid, string mode, int minUser, int maxUser, int matchTimeout, string containerImage, string containerParam, Port[] containerPorts)
+    {
+        UID = uid;
+        Mode = mode;
+        ContainerImage = containerImage;
+        ContainerParam = containerParam;
+        ContainerPorts = containerPorts;
+        MatchTimeout = matchTimeout;
+        MinUser = minUser;
+        MaxUser = maxUser;
+    }
+}
+
