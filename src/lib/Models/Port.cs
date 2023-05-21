a6937174b3a10fe9287a51462bf3c62e300e4f91
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:31:44 +0200

    Create Port.cs

---
+++ src/lib/Models/Port.cs
@@ -0,0 +1,13 @@
+﻿namespace Sisma.Models;
+
+public class Port
+{
+    public string Name { get; set; }
+    public int Value { get; set; }
+
+    public Port(string name, int value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
