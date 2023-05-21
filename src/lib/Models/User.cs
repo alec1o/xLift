49bd0b23cf6f1b42ac907b43dc66d7c5faf25077
Alecio Furanze <alecio@kezero.com>
2023-05-21 20:31:40 +0200

    Create User.cs

---
+++ src/lib/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Sisma.Models;
+
+public class User
+{
+    public string UID { get; set; }
+    public int Level { get; set; }
+    public string Token { get; set; }
+    public bool SuperUser { get; set; }
+
+    public User(string uid, string token, int level, bool superUser)
+    {
+        UID = uid;
+        Level = level;
+        Token = token;
+        SuperUser = superUser;
+    }
+}
+
