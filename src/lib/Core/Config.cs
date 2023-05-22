3f4ea84628c435b5d0cd111f7abc4259566d2088
Alecio Furanze <alecio@kezero.com>
2023-05-22 22:58:13 +0200

    require SISMA_JWT_KEY on config

--- src/lib/Core/Config.cs
+++ src/lib/Core/Config.cs
@@ -5,4 +5,8 @@ public static class Config
     public static readonly string SISMA_ROOT_KEY =
         Environment.GetEnvironmentVariable("SISMA_ROOT_KEY", EnvironmentVariableTarget.User)
         ?? throw new ArgumentNullException("SISMA_ROOT_KEY Not found");
+    
+    public static readonly string SISMA_JWT_KEY =
+        Environment.GetEnvironmentVariable("SISMA_JWT_KEY", EnvironmentVariableTarget.User)
+        ?? throw new ArgumentNullException("SISMA_JWT_KEY Not found");
 }
