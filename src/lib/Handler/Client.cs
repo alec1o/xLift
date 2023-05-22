ca9ab9022ef50cac6d3f48a82464ec3d4a76404d
Alecio Furanze <alecio@kezero.com>
2023-05-22 23:08:15 +0200

    ``update`` impl auth

--- src/lib/Handler/Client.cs
+++ src/lib/Handler/Client.cs
@@ -1,4 +1,7 @@
-﻿using Sisma.Models;
+﻿using Newtonsoft.Json;
+using Sisma.Core;
+using Sisma.Models;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.WebSockets;
 using System.Text;
 using WatsonWebsocket;
@@ -7,6 +10,8 @@ namespace Sisma.Handler
 {
     public class Client
     {
+        public const string KEY = "sisma";
+
         public User User { get; set; }
         public Match? Match { get; set; }
         public ConnectionEventArgs Connection { get; set; }
@@ -17,6 +22,19 @@ namespace Sisma.Handler
             User = user;
             Server = server;
             Connection = connection;
+        }
+
+        private void Init()
+        {
+            Dictionary<string, string> response = new Dictionary<string, string>();
+
+            string message = (User.SuperUser) ? "AUTH_ROOT_SUCCESS" : "AUTH_USER_SUCCESS";
+
+            response.Add(KEY, message);
+
+            string json = JsonConvert.SerializeObject(response);
+
+            Send(json);
         }
 
         public void Send(byte[] buffer)
@@ -31,7 +49,81 @@ namespace Sisma.Handler
 
         public static void Auth(ConnectionEventArgs connection, Server server)
         {
+            string token = connection.HttpRequest.Headers.Get("token") ?? string.Empty;
 
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                server.Socket.DisconnectClient(connection.Client.Guid);
+                return;
+            }
+
+            User user;
+
+            if (token == Config.SISMA_ROOT_KEY)
+            {
+                user = new User(uid: "", token: token, level: 0, superUser: true);
+
+                Output.Show("[ROOT USER CONNECTED]");
+                Output.Show($"{nameof(token)} -> {token}");
+
+                Output.Show($"{nameof(Config.SISMA_ROOT_KEY)} -> {Config.SISMA_ROOT_KEY}");
+            }
+            else
+            {
+                var password = Encoding.ASCII.GetBytes(Config.SISMA_JWT_KEY);
+
+                try
+                {
+                    string json = Jose.JWT.Decode(token, password);
+
+                    if (string.IsNullOrWhiteSpace(json))
+                    {
+                        throw new Exception("Invalid token");
+                    }
+
+                    Output.Show("Token: " + json);
+
+                    var content = JsonConvert.DeserializeObject<TokenContent>(json);
+
+                    if (content == null)
+                    {
+                        throw new Exception("Invalid token body");
+                    }
+
+                    int timestamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+
+                    if (content.exp < timestamp)
+                    {
+                        throw new Exception($"Invalid expired: min value is {timestamp}");
+                    }
+
+                    Output.Show(timestamp);
+
+                    user = new User(content.sub, token, content.level, false);
+                }
+                catch (Exception e)
+                {
+                    Output.Show(e);
+                    server.Socket.DisconnectClient(connection.Client.Guid);
+                    return;
+                }
+            }
+
+            // create client instance            
+            Client client = new Client(user, connection, server);
+
+            // add client on list of clients
+            server.Clients.Add(client);
+
+            client.Init();
+        }
+
+        private class TokenContent
+        {
+            [AllowNull]
+            public string sub { get; set; }
+            public int level { get; set; }
+            public long exp { get; set; }
         }
     }
 }
