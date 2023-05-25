42eb0fa10c3fa5de013514543033d79a0e0cfeed
Alecio Furanze <alecio@kezero.com>
2023-05-25 05:25:10 +0200

    ``improve`` re-create Match.cs

---
+++ src/lib/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿namespace Sisma.Models;
+
+public class Match
+{
+    public string UID { get; set; }
+    public bool Opened { get; set; }
+    public Host Host { get; set; }
+    public User[] Users { get; set; }
+
+    public Match(string uid, bool opened, Host host, User[] users)
+    {
+        this.UID = uid;
+        this.Opened = opened;
+        this.Host = host;
+        this.Users = users;
+    }
+}
