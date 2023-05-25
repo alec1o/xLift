f3e16bf4678cc98f9a916b2914bac678d7da51f5
Alecio Furanze <alecio@kezero.com>
2023-05-25 05:24:28 +0200

    Create Host.cs

---
+++ src/lib/Models/Host.cs
@@ -0,0 +1,17 @@
+﻿namespace Sisma.Models;
+
+public class Host
+{
+    public string Mode { get; set; }
+    public string RoomUID { get; set; }
+    public string Address { get; set; }
+    public List<Port> Ports { get; set; }
+
+    public Host(string address, List<Port> ports, string mode, string roomUID)
+    {
+        this.Address = address;
+        this.Ports = ports;
+        this.Mode = mode;
+        this.RoomUID = roomUID;
+    }
+}
