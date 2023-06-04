9e0ef9e11b9b41e48c68d4275588abefdcf502eb
Alecio Furanze <alecio@kezero.com>
2023-06-04 15:07:04 +0200

    add more link

--- console/src/pages/dashboard/index.tsx
+++ console/src/pages/dashboard/index.tsx
@@ -1,7 +1,8 @@
 import style from "./styles.module.css"
 import { BiUserPin, BiJoystick, BiPackage } from "react-icons/bi"
 import { FiCpu } from "react-icons/fi"
-import { VscPulse, VscGlobe } from "react-icons/vsc"
+import { SiGitbook } from "react-icons/si"
+import { VscPulse, VscGlobe, VscGithubInverted } from "react-icons/vsc"
 
 export default function Dashboard() {
     return (
@@ -21,9 +22,18 @@ export default function Dashboard() {
                     <section className={style.MenuTab}>
                         <span className={style.MenuTabName}>Internal</span>
 
-                        <a className={style.MenuTabLink} href="#">< BiPackage /> Prefab</a>
+                        <a className={style.MenuTabLink} href="#">< BiPackage /> Prefabs</a>
                         <a className={style.MenuTabLink} href="#"><VscGlobe /> Cluster</a>
                         <a className={style.MenuTabLink} href="#"><FiCpu /> Setting</a>
+                    </section>
+                    <section id={style.poweredby} className={style.MenuTab}>
+                        <section className={style.MenuTab}>
+                            <span className={style.MenuTabName}>More</span>
+                            <a className={style.MenuTabLink} href="#">< VscGithubInverted /> GitHub</a>
+                            <a className={style.MenuTabLink} href="#">< SiGitbook /> Documentation</a>
+                        </section>
+
+                        <a className={`${style.MenuTabLink} ${style.LED}`} href="#">Powered by @alec1o</a>
                     </section>
                 </nav>
             </menu>
