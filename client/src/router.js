361fe4c47bc09ce296769a8af414b2f149e3152b
Alecio Furanze <alecio@kezero.com>
2023-06-11 10:23:22 +0200

    require docker environment variables

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,13 @@ const express = require("express")
 const authMiddleware = require("./middleware/auth")
 const router = express.Router()
 
+const DOCKER_HOST = process.env.DOCKER_HOST
+const DOCKER_PORT = process.env.DOCKER_PORT
+
+if (!DOCKER_HOST) throw new Error("DOCKER_HOST Not founded")
+if (!DOCKER_PORT) throw new Error("DOCKER_PORT Not founded")
+
+
 const handler = (req, res) => {
     res.status(200).json({ "message": "hello world" })
 }
