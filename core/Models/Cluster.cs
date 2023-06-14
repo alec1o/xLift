ae82cc5edba1264a6d23facbfc725a96da9bfd90
Alecio Furanze <alecio@kezero.com>
2023-06-14 12:07:57 +0200

    Create Cluster.cs

---
+++ core/Models/Cluster.cs
@@ -0,0 +1,21 @@
+﻿namespace Sisma.Models;
+
+public class Cluster
+{
+    public string id;
+    public string host;
+    public string key;
+    public int port;
+    public int ram;
+    public int storage;
+
+    public Cluster(string id, string host, string key, int port, int ram, int storage)
+    {
+        this.id = id;
+        this.host = host;
+        this.key = key;
+        this.port = port;
+        this.ram = ram;
+        this.storage = storage;
+    }
+}
