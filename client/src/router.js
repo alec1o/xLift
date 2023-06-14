751435e73f5ffc06c82d167ebb24718ea55429f9
Alecio Furanze <alecio@kezero.com>
2023-06-14 13:56:04 +0200

    impl /sisma rote (return server info: cpu %, ram, storage (drive))

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,12 @@ const express = require("express")
 const authMiddleware = require("./middleware/auth")
 const { default: axios } = require("axios")
 const router = express.Router()
+const osu = require('node-os-utils')
+const checkDiskSpace = require('check-disk-space').default
+const opsys = process.platform;
+
+let data = {}
+
 
 const DOCKER_HOST = process.env.SISMA_DOCKER_HOST
 const DOCKER_PORT = process.env.SISMA_DOCKER_PORT
@@ -46,11 +52,59 @@ const handler = async (req, res, method) => {
     }
 }
 
+router.get("/sisma", authMiddleware, async (req, res) => {
+    res.status(200).json(data)
+});
+
+
 router.get("*", authMiddleware, async (req, res) => handler(req, res, "GET"))
 router.post("*", authMiddleware, async (req, res) => handler(req, res, "POST"))
 router.delete("*", authMiddleware, async (req, res) => handler(req, res, "DELETE"))
 router.put("*", authMiddleware, async (req, res) => handler(req, res, "PUT"))
 router.patch("*", authMiddleware, async (req, res) => handler(req, res, "PATCH"))
 router.head("*", authMiddleware, async (req, res) => handler(req, res, "HEAD"))
+
+
+async function info() {
+    const memory = await osu.mem.info();
+    const cpu = await osu.cpu.free();
+
+    let path = "/"
+
+    if ((opsys.toLowerCase()).includes("win")) {
+        path = "C:\\"
+    }
+
+    const storage = await checkDiskSpace(path)
+
+    data = {
+        "ram": mbToGb(memory.totalMemMb),
+        "ram_free": mbToGb(memory.freeMemMb),
+        "ram_used": mbToGb(memory.usedMemMb),
+
+        "storage": bytesToGb(storage.size),
+        "storage_free": bytesToGb(storage.free),
+        "storage_used": bytesToGb(storage.size - storage.free),
+
+        "cpu": cpu ? ((100 - cpu) / 100).toFixed(2) : 0,
+    }
+
+    console.log(data)
+    return data
+}
+
+function mbToGb(mbs) {
+    return (mbs / 1024).toFixed(2)
+}
+
+function bytesToGb(bytes) {
+    return (((bytes / 1024 /*byte -> kb*/) / 1024 /* kb -> mb */) / 1024 /* mb -> gb */).toFixed(2)
+}
+
+info();
+
+setInterval(() => {
+    info();
+}, 1000 * 60)
 
 module.exports = router
