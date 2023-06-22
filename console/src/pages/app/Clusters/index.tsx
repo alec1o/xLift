929b14e19f0bcafdcf47f1f1d39dbdcff219a988
Alecio Furanze <alecio@kezero.com>
2023-06-22 03:44:05 +0200

    register cluster

--- console/src/pages/app/Clusters/index.tsx
+++ console/src/pages/app/Clusters/index.tsx
@@ -6,15 +6,15 @@ import { FaMemory } from "react-icons/fa";
 import { BsCloudFill } from "react-icons/bs";
 import { MdSignalWifi4Bar as WifiOn, MdSignalWifiBad as WifiOff } from "react-icons/md";
 import { useState } from "react";
-import { globalAgent } from "http";
 
 export default function Clusters(props: any) {
     const clusters = props.clusters as { id: string, ram: number, key: string, port: number, name: string, host: string, storage: number }[]
-    const [selected, setSelected] = useState('')
-    const [selectData, setSelectData] = useState({} as { id: string, ram: number, key: string, port: number, name: string, host: string, storage: number })
+    const [selectedCluster, setSelectedCluster] = useState('')
+    const [clusterData, setClusterData] = useState({} as { id: string, ram: number, key: string, port: number, name: string, host: string, storage: number })
     const [name, setName] = useState('')
     const [address, setAddress] = useState('')
     const [port, setPort] = useState(0)
+    const [key, setKey] = useState('')
 
     function select(id: string) {
         let data = {} as { id: string, ram: number, key: string, port: number, name: string, host: string, storage: number }
@@ -25,8 +25,8 @@ export default function Clusters(props: any) {
             }
         })
 
-        setSelectData(data)
-        setSelected(id)
+        setClusterData(data)
+        setSelectedCluster(id)
     }
 
     console.log(clusters)
@@ -59,43 +59,70 @@ export default function Clusters(props: any) {
                             Worker
                             <button className={style.createColor} onClick={e => {
                                 e.preventDefault()
-                                setSelected('')
+                                setSelectedCluster('')
                             }}><TbCirclePlus /></button>
-                            {(selected) ?
+                            {(selectedCluster) ?
                                 <button className={style.deleteColor} onClick={(e) => {
                                     e.preventDefault()
-                                    setSelected('')
+                                    setSelectedCluster('')
                                     // @ts-ignore
                                     // destroy 
                                     const ws = global.ws as WebSocket
 
                                     ws.send(JSON.stringify({
                                         sisma: "CLUSTER.REMOVE",
-                                        id: selectData.id
+                                        id: clusterData.id
                                     }))
 
                                     setTimeout(() => {
                                         ws.send(JSON.stringify({
                                             sisma: "CLUSTER.SHOWALL"
                                         }))
-                                    }, 1000)
+                                    }, 100)
 
                                 }}><AiFillDelete /></button>
                                 : <></>
                             }
                         </h2>
-                        {(selected) ?
+                        {(selectedCluster) ?
                             <>
-                                <input type="text" value={selectData.name} placeholder="worker name" />
-                                <input type="text" value={selectData.host} placeholder="ip address (ipv4/ipv6/domain)" />
-                                <input type="text" value={selectData.port} placeholder="port" />
-                                <input type="text" value={selectData.ram} placeholder="ram (gb)" />
-                                <input type="text" value={selectData.storage} placeholder="storage (gb)" />
+                                <input type="text" value={clusterData.name} placeholder="worker name" />
+                                <input type="text" value={clusterData.host} placeholder="ip address (ipv4/ipv6/domain)" />
+                                <input type="text" value={clusterData.port} placeholder="port" />
+                                <input type="text" value={clusterData.ram} placeholder="ram (gb)" />
+                                <input type="text" value={clusterData.storage} placeholder="storage (gb)" />
                             </> : <>
                                 <input required onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="worker name" minLength={3} />
                                 <input required onChange={(e) => setAddress(e.target.value)} value={address} type="text" placeholder="ip address (ipv4/ipv6/domain)" minLength={3} />
                                 <input required onChange={(e) => setPort(Number.parseInt(e.target.value))} value={port > 0 ? port : ''} type="number" placeholder="port" min={1} />
-                                <input type="submit" value="Register" />
+                                <input required onChange={(e) => setKey(e.target.value)} value={key} type="text" placeholder="sisma key" minLength={3} />
+                                <input type="submit" value="Register" onClick={_ => {
+                                    _.preventDefault()
+
+                                    // @ts-ignore
+                                    // destroy 
+                                    const ws = global.ws as WebSocket
+
+                                    ws.send(JSON.stringify({
+                                        sisma: "CLUSTER.ADD",
+                                        host: address,
+                                        name: name,
+                                        port: port,
+                                        key: key,
+                                    }))
+
+                                    setTimeout(() => {
+                                        ws.send(JSON.stringify({
+                                            sisma: "CLUSTER.SHOWALL"
+                                        }))
+                                    }, 3000)
+
+                                    setAddress('')
+                                    setKey('')
+                                    setName('')
+                                    setPort(0)
+                                    setSelectedCluster('')
+                                }} />
                             </>
                         }
                     </div>
