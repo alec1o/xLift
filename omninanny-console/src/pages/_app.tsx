7a73f2bd8580b59af1a6ab97ffb29c4766db0099
Alecio Furanze <alecio@kezero.com>
2023-11-05 21:43:52 +0200

    import default font's

--- omninanny-console/src/pages/_app.tsx
+++ omninanny-console/src/pages/_app.tsx
@@ -10,6 +10,8 @@ export default function App({ Component, pageProps }: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="description" content="Omninanny - Matchmaking, Game server manager with docker" />
                 <link rel="icon" href="/favicon.ico" />
+                <link href="https://fonts.cdnfonts.com/css/kiona-2" rel="stylesheet" />
+                <link href="https://fonts.cdnfonts.com/css/rawson-alt" rel="stylesheet" />
             </Head>
             <Component {...pageProps} />
         </>
