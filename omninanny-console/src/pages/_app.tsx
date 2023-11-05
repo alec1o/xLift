a1822c798db16ec7e55cd1fbe5b65a3fd084d7da
Alecio Furanze <alecio@kezero.com>
2023-11-05 19:45:12 +0200

    ``update`` tab ``2 spaces`` to ``4 spaces``

--- omninanny-console/src/pages/_app.tsx
+++ omninanny-console/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Omninanny</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Omninanny - Matchmaking, Game server manager with docker" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
