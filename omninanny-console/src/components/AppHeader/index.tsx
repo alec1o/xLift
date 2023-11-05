008abb3f2625aa62f234884c0fe71643fe5da7fa
Alecio Furanze <alecio@kezero.com>
2023-11-05 23:13:46 +0200

    add links on header

--- omninanny-console/src/components/AppHeader/index.tsx
+++ omninanny-console/src/components/AppHeader/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import styles from "./styles.module.css"
 import Image from "next/image"
 
@@ -5,8 +6,14 @@ export default function AppHeader() {
     return (
         <>
             <header id={styles.main}>
-                <Image id={styles.logo} src="/www/image/omninanny.png" alt="omninanny logo" width={128} height={128} />
-                <h2 id={styles.title}>Omninanny</h2>
+                <div className={styles.container}>
+                    <Image id={styles.logo} src="/www/image/omninanny.png" alt="omninanny logo" width={128} height={128} />
+                    <h2 id={styles.title}>Omninanny</h2>
+                </div>
+                <div className={styles.container}>
+                    <Link className={styles.link} target="_blank" href="https://omninanny.docs.kezero.com/">Docs</Link>
+                    <Link className={styles.link} target="_blank" href="https://github.com/alec1o/omninanny/">GitHub</Link>
+                </div>
             </header>
         </>
     )
