fda5d773333a3cbe87f68235eed2532cfa0e7605
Alecio Furanze <alecio@kezero.com>
2023-11-05 21:43:10 +0200

    create app-side-bar

---
+++ omninanny-console/src/components/AppSideBar/index.tsx
@@ -0,0 +1,27 @@
+import MenuOptions from "@/ultils/MenuOptions";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+interface IProps {
+    page: MenuOptions,
+    setProp: (page: MenuOptions) => unknown
+}
+
+export default function AppSideBar({ page, setProp }: IProps) {
+
+    function buttonStyle(index: MenuOptions) {
+        return `${styles.button} ${(page == index) ? styles.selected : ''}`
+    }
+
+    return (
+        <>
+            <aside id={styles.main}>
+                <button className={buttonStyle(MenuOptions.Status)} onClick={_ => setProp(MenuOptions.Status)}>Status</button>
+                <button className={buttonStyle(MenuOptions.Server)} onClick={_ => setProp(MenuOptions.Server)}>Servers</button>
+                <button className={buttonStyle(MenuOptions.Worker)} onClick={_ => setProp(MenuOptions.Worker)}>Workers</button>
+                <button className={buttonStyle(MenuOptions.Instance)} onClick={_ => setProp(MenuOptions.Instance)}>Instances</button>
+                <Link id={styles.copyright} target="_blank" href="https://www.alec1o.com/">@alec1o</Link>
+            </aside >
+        </>
+    )
+}
