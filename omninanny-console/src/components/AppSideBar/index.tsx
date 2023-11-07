b426e634edaed1950e71bc516f2ebf96c8cf4712
Alecio Furanze <alecio@kezero.com>
2023-11-07 08:50:52 +0200

    use react-icons

--- omninanny-console/src/components/AppSideBar/index.tsx
+++ omninanny-console/src/components/AppSideBar/index.tsx
@@ -1,6 +1,7 @@
 import MenuOptions from "@/utils/MenuOptions";
 import styles from "./styles.module.css";
 import Link from "next/link";
+import { FleetIcon, ServerIcon, StatusIcon, WorkerIcon } from "@/utils/icons";
 
 interface IProps {
     page: MenuOptions,
@@ -16,10 +17,10 @@ export default function AppSideBar({ page, setProp }: IProps) {
     return (
         <>
             <aside id={styles.main}>
-                <button className={buttonStyle(MenuOptions.Status)} onClick={_ => setProp(MenuOptions.Status)}>Status</button>
-                <button className={buttonStyle(MenuOptions.Server)} onClick={_ => setProp(MenuOptions.Server)}>Servers</button>
-                <button className={buttonStyle(MenuOptions.Worker)} onClick={_ => setProp(MenuOptions.Worker)}>Workers</button>
-                <button className={buttonStyle(MenuOptions.Fleet)} onClick={_ => setProp(MenuOptions.Fleet)}>Fleets</button>
+                <button className={buttonStyle(MenuOptions.Status)} onClick={_ => setProp(MenuOptions.Status)}><StatusIcon className={styles.icon}/> Status</button>
+                <button className={buttonStyle(MenuOptions.Server)} onClick={_ => setProp(MenuOptions.Server)}><ServerIcon className={styles.icon}/> Servers</button>
+                <button className={buttonStyle(MenuOptions.Worker)} onClick={_ => setProp(MenuOptions.Worker)}><WorkerIcon className={styles.icon}/> Workers</button>
+                <button className={buttonStyle(MenuOptions.Fleet)} onClick={_ => setProp(MenuOptions.Fleet)}><FleetIcon    className={styles.icon}/> Fleets</button>
                 <Link id={styles.copyright} target="_blank" href="https://www.alec1o.com/">@alec1o</Link>
             </aside >
         </>
---
+++ omninanny-console/src/utils/icons.tsx
@@ -0,0 +1,6 @@
+import { GiProgression as StatusIcon } from "react-icons/gi"
+import { FaServer as ServerIcon } from "react-icons/fa"
+import { BsStack as FleetIcon } from "react-icons/bs"
+import { BsCpuFill as WorkerIcon } from "react-icons/bs"
+
+export { ServerIcon, StatusIcon, FleetIcon, WorkerIcon }
