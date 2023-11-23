15deeef7809de976898f4b3000d436cf366bf5d3
Alecio Furanze <alecio@kezero.com>
2023-11-23 15:40:44 +0200

    init express server

--- kuphy-worker/src/main.ts
+++ kuphy-worker/src/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello From TypeScript!");
+import APP from "./server/app";
+import ENV from "./server/env";
+import router from "./server/router";
+
+APP.use(router)
+
+APP.listen(ENV.PORT, ENV.HOST, () => {
+    console.log(`\n${ENV.APP_NAME}\n\tHOST:${ENV.HOST}\n\tPORT:${ENV.PORT}\n\tURI: http://${ENV.HOST}:${ENV.PORT}`)
+})
