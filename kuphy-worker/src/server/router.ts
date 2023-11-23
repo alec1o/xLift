36c15c089af09adf069a66f98655958d9813010a
Alecio Furanze <alecio@kezero.com>
2023-11-23 15:40:18 +0200

    Create router.ts

---
+++ kuphy-worker/src/server/router.ts
@@ -0,0 +1,11 @@
+import express, { Router } from "express"
+import RootController from "./controller/RootController"
+
+const router: Router = Router()
+
+router.use(express.json())
+
+router.get("/", new RootController(router).all)
+
+
+export default router
