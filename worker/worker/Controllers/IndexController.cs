169eb20d4b3e3903ee1a7c62181038787a3e955e
Alecio Furanze <alecio@kezero.com>
2024-04-26 19:35:54 +0200

    Create IndexController.cs

---
+++ worker/worker/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace worker.Controllers;
+
+[Route("/")]
+[ApiController]
+public class IndexController : Controller
+{
+    public record ApplicationDetails(string Name, string Version, string Documentation);
+    
+    [HttpGet]
+    public ApplicationDetails Get()
+    {
+        return new ApplicationDetails
+        (
+            Name: "xLift Worker",
+            Version: "0.1.0",
+            Documentation: "/docs"
+        );
+    }
+}
